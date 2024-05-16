1f3eda406fdfd3fb71f26d8a3fbd616037f6fbb1
redheadinthepark <ada16037@byui.edu>
2024-05-15 20:04:32 -0600

    chamber updaes

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -1,8 +1,3 @@
-const currentYear = new Date().getFullYear();
-
-const footerFirstParagraph = document.querySelector('footer p:first-of-type');
-footerFirstParagraph.textContent = `©${currentYear} Leah Adair United States of America`;
-
 const lastModified = document.lastModified;
 const footerSecondParagraph = document.querySelector('footer p:last-of-type');
 footerSecondParagraph.textContent = `Last modified ${lastModified}`;
