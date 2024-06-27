a1777e157a72c066f5706e64bff06e0067d48208
redheadinthepark <ada16037@byui.edu>
2024-06-26 20:40:33 -0600

    changes lesson 10

--- scripts/weather.js
+++ scripts/weather.js
@@ -2,7 +2,7 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIconContainer = document.querySelector('#weather-icons');
 const captionDesc = document.querySelector('figcaption');
 
-const url = "https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid=be0a5249ffe06607b565d0f2bc96df80";
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=40.63&lon=-112.30&appid=be0a5249ffe06607b565d0f2bc96df80";
 
 async function apiFetch() {
     try {
@@ -26,10 +26,9 @@ function capitalizeDescription(description) {
 }
 
 function displayResults(data) {
-    const tempFahrenheit = ((data.main.temp - 273.15) * 9/5 + 32).toFixed(0); // Convert Kelvin to Fahrenheit and format to zero decimal points
+    const tempFahrenheit = ((data.main.temp - 273.15) * 9/5 + 32).toFixed(0);
     currentTemp.innerHTML = `${tempFahrenheit}&deg;F`;
 
-    // Clear any existing weather icons and descriptions
     weatherIconContainer.innerHTML = '';
     captionDesc.textContent = '';
 
@@ -37,15 +36,13 @@ function displayResults(data) {
         const iconsrc = `https://openweathermap.org/img/wn/${event.icon}@2x.png`;
         let desc = capitalizeDescription(event.description);
 
-        // Create img element for weather icon
         const weatherIcon = document.createElement('img');
         weatherIcon.setAttribute('src', iconsrc);
         weatherIcon.setAttribute('alt', desc);
 
-        // Append weather icon to container
         weatherIconContainer.appendChild(weatherIcon);
 
-        // Append description to figcaption
+
         const descSpan = document.createElement('span');
         descSpan.textContent = `${desc}`;
         captionDesc.appendChild(descSpan);
